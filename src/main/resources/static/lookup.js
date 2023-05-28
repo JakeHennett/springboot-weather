93d4def16302255912ca78e872bc6116ef1a14ee
Jake Hennett <jake.hennett@gmail.com>
2023-05-28 16:39:07 -0400

    It works. Just make sure you use a valid GPS lat and long

--- src/main/resources/static/lookup.js
+++ src/main/resources/static/lookup.js
@@ -1,13 +1,3 @@
-//add some scripting logic here to make the button work
-
-
-function submit() {
-    var response = "Searching for current weather at " + document.getElementById("latitude").value
-        + "," + document.getElementById("longitude").value + "..."
-    alert(response);
-    //Call into endpoint methods here
-}
-
 async function getForecast(latitude, longitude) {
     let forecastRequestString = "http://localhost:8080/weather/forecast/" + latitude + "/" + longitude;
 
@@ -25,29 +15,4 @@ async function getForecast(latitude, longitude) {
     let response = await result.json();
     let forecast = response.properties.periods[0].detailedForecast;
     alert(forecast);
-}
-
-
-//example async function. mimic this functionality.
-async function clickedEvent(art_index, info_index) {
-    //  Get Art Id
-    let id = document.getElementsByTagName('img')[art_index].attributes[2].value;
-
-    let headers = new Headers([
-        ['Content-Type', 'application/json'],
-        ['Accept', 'application/json']
-    ]);
-
-    let request = new Request(`https://api.artic.edu/api/v1/artworks/${id}? 
-  fields=id,title,artist_display,date_display,main_reference_number`, {
-        method: 'GET',
-        headers: headers
-    });
-    let result = await fetch(request);
-    let response = await result.json();
-    console.log(response)
-
-    if (showArtInfo) {
-        stopShow();
-    }
 }
