7a1473c8ef00586e4c331b77f33018a5983cfddd
Jake Hennett <jake.hennett@gmail.com>
2023-05-27 21:24:18 -0400

    /weather/points will return a response. needs query params

--- src/main/java/com/snhu/weather/RestWeatherController.java
+++ src/main/java/com/snhu/weather/RestWeatherController.java
@@ -1,7 +1,9 @@
 package com.snhu.weather;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RestWeatherController {
@@ -9,5 +11,30 @@ public class RestWeatherController {
     @RequestMapping("/test")
     public String test() {
         return "test string output";
+    }
+
+    @GetMapping(value = "/callclienthello")
+    private String getHelloClient() {
+        String uri = "http://localhost:8080/test";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping(value = "/scryfall")
+    private String getScryfallRandom() {
+        String uri = "https://api.scryfall.com/cards/random";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    //TODO: make this accept query parameters
+    @GetMapping(value = "/weather/points")
+    private String getPointsByCoordinates() {
+        String uri = "https://api.weather.gov/points/35,-82";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
     }
 }
