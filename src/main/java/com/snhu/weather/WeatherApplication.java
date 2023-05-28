8a6842124a399daee2df573d09c663b14eb185cf
Jake Hennett <jake.hennett@gmail.com>
2023-05-27 20:08:50 -0400

    Add some basic endpoint mapping in springboot

--- src/main/java/com/snhu/weather/WeatherApplication.java
+++ src/main/java/com/snhu/weather/WeatherApplication.java
@@ -2,12 +2,24 @@ package com.snhu.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/error")
+	public String error() {
+		return String.format("Oops.");
 	}
 
 }
