2ed4935a3d15dbc8307405856d7f7f682636aa7d
Jake Hennett <jake.hennett@gmail.com>
2023-05-28 08:53:00 -0400

    Rest Controller provides long and lat as uri params

--- src/main/java/com/snhu/weather/RestWeatherController.java
+++ src/main/java/com/snhu/weather/RestWeatherController.java
@@ -1,6 +1,7 @@
 package com.snhu.weather;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -29,13 +30,41 @@ public class RestWeatherController {
         return result;
     }
 
-    //TODO: make this accept query parameters
-    @GetMapping(value = "/weather/points")
-    private String getPointsByCoordinates() {
-        String uri = "https://api.weather.gov/points/35,-82";
+    //http://localhost:8080/weather/points/31/-82
+    @GetMapping(value = "/weather/points/{longitude}/{latitude}")
+    private String getPointsByCoordinates(@PathVariable Long longitude, @PathVariable Long latitude) {
+        String uri = "https://api.weather.gov/points/" + longitude + "," + latitude;
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         return result;
     }
+
+    //Below paths are copied from the quotes example
+    //https://github.com/spring-guides/quoters/blob/master/src/main/java/org/springframework/quoters/QuoteController.java
+    
+	// @GetMapping("/api")
+	// public List<QuoteResource> getAll() {
+
+	// 	return repository.findAll().stream()
+	// 		.map(quote -> new QuoteResource(quote, "success"))
+	// 		.collect(Collectors.toList());
+	// }
+
+	// @GetMapping("/api/{id}")
+	// public QuoteResource getOne(@PathVariable Long id) {
+
+	// 	return repository.findById(id)
+	// 		.map(quote -> new QuoteResource(quote, "success"))
+	// 		.orElse(new QuoteResource(NONE, "Quote " + id + " does not exist"));
+	// }
+
+	// @GetMapping("/api/random")
+	// public QuoteResource getRandomOne() {
+	// 	return getOne(nextLong(1, repository.count() + 1));
+	// }
+
+	// private long nextLong(long lowerRange, long upperRange) {
+	// 	return (long) (RANDOMIZER.nextDouble() * (upperRange - lowerRange)) + lowerRange;
+	// }
 
 }
